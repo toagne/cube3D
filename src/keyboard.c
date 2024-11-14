83715286845c2b3de3356afd95fb7a9d7a0bf630
Giulia Micoli <giuls@Abulafia.local>
2024-11-14 23:16:25 +0200

    added wall collision for s key and partially working for d key

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   keyboard.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpellegr <mpellegr@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: giuls <giuls@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 10:38:34 by mpellegr          #+#    #+#             */
-/*   Updated: 2024/11/14 16:43:56 by mpellegr         ###   ########.fr       */
+/*   Updated: 2024/11/14 23:12:15 by giuls            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,29 @@ void	ft_keyboard(mlx_key_data_t keydata, void *param)
 	avoid_wall_collision = 20;
 	int mpy = table->player_y / T_SIZE;
 	int mpx = table->player_x / T_SIZE;
-	int mpyc, mpxc;
+	int mpycw, mpxcw;
 	if (table->player_angle > 0 && table->player_angle < 180)
-		mpyc = (table->player_y + avoid_wall_collision) / T_SIZE;
+		mpycw = (table->player_y + avoid_wall_collision) / T_SIZE;
 	else
-		mpyc = (table->player_y - avoid_wall_collision) / T_SIZE;
-	if (table->player_angle > 270 || table->player_angle < 90)
-		mpxc = (table->player_x + avoid_wall_collision) / T_SIZE;
+		mpycw = (table->player_y - avoid_wall_collision) / T_SIZE;
+	if (table->player_angle >= 270 || table->player_angle < 90)
+		mpxcw = (table->player_x + avoid_wall_collision) / T_SIZE;
 	else
-		mpxc = (table->player_x - avoid_wall_collision) / T_SIZE;
+		mpxcw = (table->player_x - avoid_wall_collision) / T_SIZE;
 	printf("mpy = %d\n", mpy);
 	printf("mpx = %d\n", mpx);
-	printf("mpyc = %d\n", mpyc);
-	printf("mpxc = %d\n\n", mpxc);
+	printf("player x = %f\n", table->player_x);
+	printf("mpyc = %d\n", mpycw);
+	printf("mpxc = %d\n\n", mpxcw);
+	int mpycs, mpxcs;
+	if (table->player_angle > 0 && table->player_angle < 180)
+		mpycs = (table->player_y - avoid_wall_collision) / T_SIZE;
+	else
+		mpycs = (table->player_y + avoid_wall_collision) / T_SIZE;
+	if (table->player_angle > 270 || table->player_angle < 90)
+		mpxcs = (table->player_x - avoid_wall_collision) / T_SIZE;
+	else
+		mpxcs = (table->player_x + avoid_wall_collision) / T_SIZE;
 	//int mpy_neg = (table->player_y - avoid_wall_collision) / T_SIZE;
 	//int mpy_pos = (table->player_y + avoid_wall_collision) / T_SIZE;
 	//int mpx_neg = (table->player_x - avoid_wall_collision) / T_SIZE;
@@ -45,20 +55,24 @@ void	ft_keyboard(mlx_key_data_t keydata, void *param)
 	}
 	if (keydata.key == MLX_KEY_W && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		if (table->map[mpy * (table->columns + 1) + mpxc] != '1')
+		if (table->map[mpy * (table->columns + 1) + mpxcw] != '1')
 			table->player_x+=table->player_delta_x * 5;
-		if (table->map[mpyc * (table->columns + 1) + mpx] != '1')
+		if (table->map[mpycw * (table->columns + 1) + mpx] != '1')
 			table->player_y+=table->player_delta_y * 5;
 	}
 	if (keydata.key == MLX_KEY_S && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		table->player_x-=table->player_delta_x * 5;
-		table->player_y-=table->player_delta_y * 5;
+		if (table->map[mpy * (table->columns + 1) + mpxcs] != '1')
+			table->player_x-=table->player_delta_x * 5;
+		if (table->map[mpycs * (table->columns + 1) + mpx] != '1')
+			table->player_y-=table->player_delta_y * 5;
 	}
 	if (keydata.key == MLX_KEY_D && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		table->player_x+=table->player_delta_x_ad * 5;
-		table->player_y+=table->player_delta_y_ad * 5;
+		if (table->map[mpy * (table->columns + 1) + mpxcw] != '1')
+			table->player_x+=table->player_delta_x_ad * 5;
+		if (table->map[mpycw * (table->columns + 1) + mpx] != '1')
+			table->player_y+=table->player_delta_y_ad * 5;
 	}
 	if (keydata.key == MLX_KEY_A && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
