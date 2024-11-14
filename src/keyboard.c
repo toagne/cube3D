2839252c8a88f7e23aa49c2ea3925c461d6445cf
Marco Pellegrini <mpellegr@c2r5p4.hive.fi>
2024-11-14 16:46:50 +0200

    added collision when pressing w

--- src/keyboard.c
+++ src/keyboard.c
@@ -6,7 +6,7 @@
 /*   By: mpellegr <mpellegr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 10:38:34 by mpellegr          #+#    #+#             */
-/*   Updated: 2024/11/14 15:52:05 by mpellegr         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:43:56 by mpellegr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,25 @@
 void	ft_keyboard(mlx_key_data_t keydata, void *param)
 {
 	t_table	*table;
-	//int	avoid_wall_collision;
+	int	avoid_wall_collision;
 
 	table = (t_table *)param;
-	//avoid_wall_collision = 20;
-	//int mpy = table->player_y / T_SIZE;
-	//int mpx = table->player_x / T_SIZE;
+	avoid_wall_collision = 20;
+	int mpy = table->player_y / T_SIZE;
+	int mpx = table->player_x / T_SIZE;
+	int mpyc, mpxc;
+	if (table->player_angle > 0 && table->player_angle < 180)
+		mpyc = (table->player_y + avoid_wall_collision) / T_SIZE;
+	else
+		mpyc = (table->player_y - avoid_wall_collision) / T_SIZE;
+	if (table->player_angle > 270 || table->player_angle < 90)
+		mpxc = (table->player_x + avoid_wall_collision) / T_SIZE;
+	else
+		mpxc = (table->player_x - avoid_wall_collision) / T_SIZE;
+	printf("mpy = %d\n", mpy);
+	printf("mpx = %d\n", mpx);
+	printf("mpyc = %d\n", mpyc);
+	printf("mpxc = %d\n\n", mpxc);
 	//int mpy_neg = (table->player_y - avoid_wall_collision) / T_SIZE;
 	//int mpy_pos = (table->player_y + avoid_wall_collision) / T_SIZE;
 	//int mpx_neg = (table->player_x - avoid_wall_collision) / T_SIZE;
@@ -32,9 +45,9 @@ void	ft_keyboard(mlx_key_data_t keydata, void *param)
 	}
 	if (keydata.key == MLX_KEY_W && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-	//	if (table->map[mpy * (table->columns + 1) + mpx_pos])
+		if (table->map[mpy * (table->columns + 1) + mpxc] != '1')
 			table->player_x+=table->player_delta_x * 5;
-	//	if (table->map[mpy_neg * (table->columns + 1) + mpx] != '1')
+		if (table->map[mpyc * (table->columns + 1) + mpx] != '1')
 			table->player_y+=table->player_delta_y * 5;
 	}
 	if (keydata.key == MLX_KEY_S && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
